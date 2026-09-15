da939c08896eed2b005128598cccfafd70e7cf9c
bbvbv444 <ogunleyeadewura43@gmail.com>
2026-09-15 11:08:39 +0100

    Update supabase.js

--- shared/supabase.js
+++ shared/supabase.js
@@ -1,4 +1,4 @@
 const SUPABASE_URL = 'https://bqnlvhtjfrttrnizjsyv.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_GwAfYVAEkQ_EZFFtC7j8Vw_rb8kkUyC';
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
